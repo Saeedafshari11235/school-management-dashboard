731ef385a7b732ddbe89223acf4bf2d38c486917
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-15 12:09:35 +0330

    create data for chart

---
+++ src/datas.js
@@ -0,0 +1,16 @@
+let chartData = [
+  { month: "Jan", sale: 50 },
+  { month: "Feb", sale: 200 },
+  { month: "Mar", sale: 800 },
+  { month: "Apr", sale: 900 },
+  { month: "May", sale: 1_500 },
+  { month: "Jun", sale: 700 },
+  { month: "Jul", sale: 500 },
+  { month: "Aug", sale: 200 },
+  { month: "Sep", sale: 1100 },
+  { month: "Oct", sale: 1500 },
+  { month: "Nov", sale: 1800 },
+  { month: "Dec", sale: 2200 },
+];
+
+export {chartData}
