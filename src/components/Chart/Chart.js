12bb15b9d5ab10503ee8db10bd66650b4f8ccb6d
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-15 12:09:25 +0330

    Add chart to the home page

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -12,9 +12,9 @@ export default function Chart({ title, data, datakey, grid }) {
   return (
     <div className="chart">
       <h3 className="charttitle">{title}</h3>
-      <ResponsiveContainer width="100%">
+      <ResponsiveContainer width="100%" aspect={4}>
         <LineChart data={data}>
-          <XAxis dataKey="name" stroke="#5550bd"></XAxis>
+          <XAxis dataKey="month" stroke="#5550bd"></XAxis>
           <Line dataKey={datakey} stroke="#5550bd"></Line>
           <Tooltip />
           {grid && <CartesianGrid stroke="#e0dfdf" strokeDasharray="10" />}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Features from "../../components/Features/Features";
+import Chart from "../../components/Chart/Chart";
 import "./Home.css";
+import { chartData } from "../../datas";
 
 export default function Home() {
   return (
     <div className="home">
       <Features />
+      <Chart grid title="Monthly sale" data={chartData} datakey="sale"/>
     </div>
   );
 }
